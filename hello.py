5486fd51ba4b35c11214bd7d1430c26bdb525ca1
Cyrus Conde <condecyrus25@gmail.com>
2022-01-27 11:12:38 +0800

    updated

--- hello.py
+++ hello.py
@@ -3,18 +3,18 @@ from flask_mysqldb import MySQL
 
 from dotenv import load_dotenv
 load_dotenv()
-from create import create
-from edit import edit
-from delete import delete
+from student import student
+from course import course
+from college import college
 import cloudinary
 import cloudinary.uploader
 import MySQLdb
 import os
 
 app = Flask(__name__)
-app.register_blueprint(create,url_prefix="/create")
-app.register_blueprint(edit,url_prefix="/edit")
-app.register_blueprint(delete,url_prefix="/delete")
+app.register_blueprint(student,url_prefix="/")
+app.register_blueprint(course,url_prefix="/")
+app.register_blueprint(college,url_prefix="/")
 mysql = MySQL(app)
 
 app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
@@ -31,7 +31,9 @@ cloudinary.config(
   api_secret = os.getenv("cloud_api_secret") 
 )
 
-
+@app.route("/create/")
+def createDashboard():
+    return render_template("create.html")
 
 
 @app.route('/searchField', methods=['GET','POST'])
@@ -60,27 +62,7 @@ def searchField():
 
 @app.route('/', methods=["POST","GET"])
 def index():
-    ##get student data
-    stud_cur = mysql.connection.cursor()
-    stud_query = "SELECT * FROM student"
-    stud_cur.execute(stud_query)
-    stud_data = stud_cur.fetchall()
-    stud_cur.close()
-    ##end of getting the student data
-    ##get college data
-    coll_cur = mysql.connection.cursor()
-    coll_query = "SELECT * FROM college"
-    coll_cur.execute(coll_query)
-    coll_data = coll_cur.fetchall()
-    coll_cur.close()
-    ## end of getting the college data
-    ##get course data
-    course_cur = mysql.connection.cursor()
-    course_query = "SELECT * FROM course"
-    course_cur.execute(course_query)
-    course_data = course_cur.fetchall()
-    course_cur.close()
-    return render_template("index.html",stud_data = stud_data,coll_data = coll_data, course_data = course_data)
+    return render_template("index.html")
 
 
 
--- student.py
+++ student.py
@@ -4,13 +4,11 @@ from datetime import date
 import MySQLdb
 import cloudinary
 import cloudinary.uploader
-create = Blueprint("create",__name__,template_folder="templates")
+student = Blueprint("student",__name__,template_folder="templates")
 app = Flask(__name__)
 mysql = MySQL(app)
 ALLOWED_EXTENSIONS = ["png", "jpg", "jpeg", "gif"]
-@create.route("/")
-def createDashboard():
-    return render_template("create.html")
+
 idnum = ""
 nnnn = 1
 
@@ -48,64 +46,66 @@ def set_nnnn():
         nnnn = 1
 def get_nnnn():
     return nnnn
-@create.route('/college/', methods=["POST","GET"])
-def create_college():
-    if request.method == "POST":
-        collegeCode = request.form['college-code']
-        collegeName = request.form['college-name']
-        cur = mysql.connection.cursor()
-        query = "INSERT INTO `college`(`code`,`name`) VALUES (%s,%s)"
-        values = (collegeCode,collegeName)
-        try:
-            if cur.execute(query, values):
-                mysql.connection.commit()
-                cur.close()
-                flash("College successfully created")
-                return redirect(url_for("create.create_college"))
-            
-        except MySQLdb.IntegrityError:
-            flash("Failed to create college")
-            return redirect(url_for("create.create_college"))
-        except:
-            flash("Failed to create college")
-            return redirect(url_for("create.create_college"))
-        
-    else:
-        return render_template("/create/college.html")
-    return render_template("/create/college.html")
 
-@create.route('/course/', methods=["POST","GET"])
-def create_course():
+@student.route("/delete/student/<value>")
+def delete(value):
+    try:
+        delete = mysql.connection.cursor()
+        del_query = "DELETE FROM student WHERE id = %s"
+        del_values = (value,)
+        
+        delete.execute(del_query,del_values)
+        mysql.connection.commit()
+        delete.close()
+        flash("Student has been deleted")
+        return redirect(url_for('index'))
+    except:
+        flash("Failed to delete student")
+        return redirect(url_for("index"))
+
+@student.route("/edit/student/<value>",methods=["POST","GET"])
+def edit(value):
     if request.method == "POST":
-        courseCode = request.form['course-code']
-        courseName = request.form['course-name']
-        collegeCode = request.form['college-code']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        course = request.form['course']
+        yearLevel = request.form['yearLevel']
+        gender = request.form['gender']
+        #image = request.files['image']
         cur = mysql.connection.cursor()
-        query = "INSERT INTO `course`(`code`,`name`,`college`) VALUES (%s,%s,%s)"
-        values = (courseCode,courseName,collegeCode)
-        try:         
-            if cur.execute(query, values):
+        edit_stud_que = "UPDATE student SET firstname=%s,lastname=%s,course=%s,yearLevel=%s,gender=%s WHERE id = %s"
+        #if image and image.filename.split(".")[-1].lower() in ALLOWED_EXTENSIONS:
+            #image_url = cloudinary.uploader.upload(image)
+            #image_url['secure_url'],
+        edit_stud_val = (firstname,lastname,course,yearLevel,gender,value)
+        try:
+            if cur.execute(edit_stud_que,edit_stud_val):
                 mysql.connection.commit()
                 cur.close()
-                flash("Course successfully created")
-                return redirect(url_for("create.create_course"))
-        except MySQLdb.IntegrityError:
-            flash("Failed to create course")
-            return redirect(url_for("create.create_course"))
+                flash("Student has been updated")
+            return redirect(url_for("index"))
         except:
-            flash("Failed to create course")
-            return redirect(url_for("create.create_course"))
+            flash("Failed to update student")
+            return redirect(url_for("index"))    
+
     else:
         cur = mysql.connection.cursor()
-        query = "SELECT code FROM college"
-        cur.execute(query)
+        cur.execute("SELECT * FROM student WHERE id = %s", (value,))
         data = cur.fetchall()
         cur.close()
-        return render_template("/create/course.html",data=data)
-    return render_template("/create/course.html")
 
-@create.route('/student/', methods=["POST","GET"])
-def create_student():
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT code FROM course")
+        coursedata = cur.fetchall()
+        cur.close()
+        if data:
+            return render_template("/edit/student.html", results=data,coursedata=coursedata)
+        else:
+            return "No Data Found"
+
+
+@student.route('/create/student/', methods=["POST","GET"])
+def create():
     
     if request.method == "POST":
         set_nnnn()
@@ -129,13 +129,13 @@ def create_student():
                     mysql.connection.commit()
                     cur.close()
                     flash("Student successfully created")
-                    return redirect(url_for("create.create_student"))
+                    return redirect(url_for("student.create"))
             except MySQLdb.IntegrityError:
                 flash("Failed to create student")
-                return redirect(url_for("create.create_student"))
+                return redirect(url_for("student.create"))
             except:
                 flash("Failed to create student")
-                return redirect(url_for("create.create_student"))
+                return redirect(url_for("student.create"))
     else:
         cur = mysql.connection.cursor()
         query = "SELECT code FROM course"
